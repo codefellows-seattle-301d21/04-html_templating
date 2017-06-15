7787c55341b5996d4ce51f151162d24782070500
sharmarke1 <sharmarkeibrahim@Sharmarke-Ibrahims-MacBook-Pro.local>
2017-06-15 16:36:37 -0700

    added the html

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -16,6 +16,8 @@ Article.prototype.toHtml = function() {
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
 
+
+
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
