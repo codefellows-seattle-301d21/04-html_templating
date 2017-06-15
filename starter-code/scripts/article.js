6a26b1454e123dc737feefc25d332cd7b0e87a1f
Benjamin <twofacebass@gmail.com>
2017-06-15 16:43:21 -0700

    finished the js TODO items

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -16,6 +16,9 @@ Article.prototype.toHtml = function() {
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
 
+var template = $('#article-template').html();
+var templateRender = Handlebars.compile(template);
+
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -24,7 +27,7 @@ Article.prototype.toHtml = function() {
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-
+  return templateRender(this);
 };
 
 rawData.sort(function(a,b) {
